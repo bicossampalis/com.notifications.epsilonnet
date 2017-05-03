b471002179e5678f6db900988eba036ba376302a
bicossampalis <beaucoupfish25@hotmail.com>
2017-05-03 17:41:04 +0300

    0.14

--- src/android/MyService.java
+++ src/android/MyService.java
@@ -7,12 +7,11 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
- import android.support.v4.app.NotificationCompat;
-import android.view.View;
-import android.widget.Button;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 
 import android.util.Log;
 
@@ -31,7 +30,7 @@ public class MyService extends BackgroundService {
 		try {
 			NotificationCompat.Builder mBuilder =
     new NotificationCompat.Builder(this)
-    .setSmallIcon(R.drawable.notification_icon)
+    .setSmallIcon(0)
     .setContentTitle("My notification")
     .setContentText("Hello World!");
 			result.put("Message", "skata");
