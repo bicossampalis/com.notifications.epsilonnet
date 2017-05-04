1a7a2078d54424a0210d702afd874e91c2cdc4d1
bicossampalis <beaucoupfish25@hotmail.com>
2017-05-04 12:59:39 +0300

    0.30

--- src/android/ResultActivity.java
+++ src/android/ResultActivity.java
@@ -3,7 +3,9 @@ package com.red_folder.phonegap.plugin.backgroundservice.sample;
 
 import android.app.Activity;
 import android.content.Intent;
-
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ResultActivity extends Activity {
 
