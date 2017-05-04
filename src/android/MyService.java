8d41e05ee3325672ed1113355690d4b248b28407
bicossampalis <beaucoupfish25@hotmail.com>
2017-05-04 15:41:32 +0300

    0.39

--- src/android/MyService.java
+++ src/android/MyService.java
@@ -20,6 +20,7 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 
