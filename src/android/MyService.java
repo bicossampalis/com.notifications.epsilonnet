acdf14a5af29b6c9bebf17a4754e7c6185d10048
bicossampalis <beaucoupfish25@hotmail.com>
2017-05-08 12:36:58 +0300

    0.90

--- src/android/MyService.java
+++ src/android/MyService.java
@@ -336,9 +336,9 @@ private final static String _LogData = "LogData";
 				} else {
 					JSONObject jsonResult = new JSONObject(jsonResponse.getString("Result"));
 					String RetrievedDataStr = jsonResult.getString("RetrievedData");
-					if(RetrievedDataStr != null)
+					if(RetrievedDataStr != null && RetrievedDataStr.length() != 0)
 					 CreateNotification(200, RetrievedDataStr);
-					 setLogData(DateTimeNow(), "ok");
+					setLogData(DateTimeNow(), "ok");
 				}
 			}	
 
