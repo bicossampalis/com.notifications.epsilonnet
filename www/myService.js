a09f6d0e4d2161a46e19d2fc4d85ba15e9878a5a
Sotiris Fotineas <beaucoupfish25@hotmail.com>
2018-12-04 10:33:24 +0200

    3.19

--- www/myService.js
+++ www/myService.js
@@ -25,5 +25,11 @@ var serviceName = 'com.red_folder.phonegap.plugin.backgroundservice.sample.MySer
  * Get an instance of the background service factory
  * Use it to create a background service wrapper for your service
  */
-var factory = require('com.red_folder.phonegap.plugin.backgroundservice.BackgroundService');
-module.exports = factory.create(serviceName);
+ 
+ 
+
+
+function createService(name){
+	var factory = require(name);
+	module.exports = factory.create(serviceName);
+}
