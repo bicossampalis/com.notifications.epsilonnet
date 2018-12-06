b1282b66f8d244b8bcc290dfbd0d669ed6618d63
Sotiris Fotineas <beaucoupfish25@hotmail.com>
2018-12-06 11:53:36 +0200

    3.30

---
+++ src/android/MyService2.java
@@ -0,0 +1,50 @@
+package com.red_folder.phonegap.plugin.backgroundservice.sample;
+
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.app.Activity;
+import android.content.Intent;
+import android.R;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import com.red_folder.phonegap.plugin.backgroundservice.BackgroundService;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.Icon;
+
+public class MyService2 extends MyService {
+
+	
+}
+
+
